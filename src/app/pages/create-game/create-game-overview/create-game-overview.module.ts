a1c0592dfbe420c0db3d1bdbcee26753ad0c131d
felixerdy <felix.erdmann@uni-muenster.de>
2019-10-11 16:01:05 +0200

    create done animation

--- src/app/pages/create-game/create-game-overview/create-game-overview.module.ts
+++ src/app/pages/create-game/create-game-overview/create-game-overview.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { CreateGameOverviewPage } from './create-game-overview.page';
+import { LottieAnimationViewModule } from 'ng-lottie';
 
 const routes: Routes = [
   {
@@ -19,7 +20,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    LottieAnimationViewModule.forRoot()
   ],
   declarations: [CreateGameOverviewPage]
 })
--- src/app/pages/create-game/create-game-overview/create-game-overview.page.ts
+++ src/app/pages/create-game/create-game-overview/create-game-overview.page.ts
@@ -11,6 +11,7 @@ import { GameFactoryService } from "../../../services/game-factory.service";
 import { PopoverComponent } from "../../../popover/popover.component";
 import { GamesService } from 'src/app/services/games.service';
 
+
 @Component({
   selector: "app-create-game-overview",
   templateUrl: "./create-game-overview.page.html",
@@ -18,13 +19,24 @@ import { GamesService } from 'src/app/services/games.service';
 })
 export class CreateGameOverviewPage implements OnInit {
   public model = this.gameFactory.getGame()
+  public lottieConfig: Object;
+  showSuccess: boolean = false
 
   constructor(
     public popoverController: PopoverController,
     public navCtrl: NavController,
     public gameFactory: GameFactoryService,
     public gamesService: GamesService
-  ) { }
+  ) {
+    this.lottieConfig = {
+      path: 'assets/lottie/astronaut.json',
+      renderer: 'canvas',
+      autoplay: true,
+      loop: true
+    };
+  }
+
+  // #0a1b28
 
   ngOnInit() { }
 
@@ -39,11 +51,15 @@ export class CreateGameOverviewPage implements OnInit {
     return await popover.present();
   }
 
-  goToCreateGameMap() {
+  uploadGame() {
     this.gameFactory.addGameInformation(this.model);
     console.log(this.gameFactory.game)
 
-    this.gamesService.postGame(this.gameFactory.game).then(res => res.json()).then(data => console.log(data))
-    this.navCtrl.navigateForward(`/`)
+    this.gamesService.postGame(this.gameFactory.game).then(res => console.log(res)).catch(e => console.error(e))
+    this.showSuccess = true
+  }
+
+  navigateHome() {
+    this.navCtrl.navigateRoot('/')
   }
 }
