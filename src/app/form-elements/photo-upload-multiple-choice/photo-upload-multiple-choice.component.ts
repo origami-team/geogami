74b5cf168e9c5579b0f244e1821758762ecbc550
felixerdy <felix.erdmann@uni-muenster.de>
2019-10-22 22:31:33 +0200

    resolve build errors

--- src/app/form-elements/photo-upload-multiple-choice/photo-upload-multiple-choice.component.ts
+++ src/app/form-elements/photo-upload-multiple-choice/photo-upload-multiple-choice.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Field } from 'src/app/dynamic-form/models/field';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PopoverController } from '@ionic/angular';
+import { PopoverComponent } from 'src/app/popover/popover.component';
 
 
 @Component({
@@ -32,7 +34,7 @@ export class PhotoUploadMultipleChoiceComponent implements Field {
 
   photo: string
 
-  constructor(private camera: Camera) { }
+  constructor(private camera: Camera, public popoverController: PopoverController) { }
 
   capturePhoto() {
     this.camera.getPicture(this.cameraOptions).then((imageData) => {
@@ -56,6 +58,16 @@ export class PhotoUploadMultipleChoiceComponent implements Field {
     }, (err) => {
       // Handle error
     });
+  }
+
+  async showPopover(ev: any, text: string) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: ev,
+      translucent: true,
+      componentProps: { text }
+    });
+    return await popover.present();
   }
 
 }
--- src/app/form-elements/photo-upload/photo-upload.component.ts
+++ src/app/form-elements/photo-upload/photo-upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Field } from 'src/app/dynamic-form/models/field';
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { PopoverComponent } from 'src/app/popover/popover.component';
+import { PopoverController } from '@ionic/angular';
 
 
 @Component({
@@ -32,7 +34,7 @@ export class PhotoUploadComponent implements Field {
 
   photo: string
 
-  constructor(private camera: Camera) { }
+  constructor(private camera: Camera, public popoverController: PopoverController) { }
 
 
   capturePhoto() {
@@ -57,6 +59,16 @@ export class PhotoUploadComponent implements Field {
     }, (err) => {
       // Handle error
     });
+  }
+
+  async showPopover(ev: any, text: string) {
+    const popover = await this.popoverController.create({
+      component: PopoverComponent,
+      event: ev,
+      translucent: true,
+      componentProps: { text }
+    });
+    return await popover.present();
   }
 
 
