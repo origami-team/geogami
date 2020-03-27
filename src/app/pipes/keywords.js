a3d653484129c3663dbe8a98b0e64ad66d8b18cf
JaninaBistron <56539845+JaninaBistron@users.noreply.github.com>
2020-03-27 16:20:19 +0100

    small fixes
    - Fensterwegklappen nicht bei Nav mit Richtungspfeil
    - Kartenfeatures nicht bei Nav mit Richtungspfeil
    - Schlüsselwörteranpassung (bitte überprüfen oder ggf. einfach nicht übernehmen) + bitte im Issue nachlesen

--- src/app/pipes/keywords.js
+++ src/app/pipes/keywords.js
@@ -8,18 +8,12 @@ export const mappings = [
     {
         text: "du bist hier",
         tag: "KEYWORD_HERE",
-        color: "warning",
-        icon: "location"
+        color: "danger",
+        icon: "radio-button-on"
     },
     {
         text: "marker",
         tag: "KEYWORD_MARKER",
-        color: "secondary",
-        icon: "location"
-    },
-    {
-        text: "markiere",
-        tag: "KEYWORD_MARK",
         color: "secondary",
         icon: "location"
     },
@@ -39,18 +33,54 @@ export const mappings = [
         text: "blickrichtung",
         tag: "KEYWORD_VIEW_DIRECTION",
         color: "secondary",
-        icon: "location"
-    },
-    {
-        text: "markierte",
-        tag: "KEYWORD_MARKED",
-        color: "secondary",
-        icon: "location"
+        icon: "caret-foreward"
     },
     {
         text: "markierte Haus",
-        tag: "KEYWORD_MARKED_HOUSE",
+        tag: "KEYWORD_MARKEDHOUSE",
         color: "secondary",
-        icon: "location"
+        icon: "cube-outline"
+    },
+    {
+        text: "markierte Gebäude",
+        tag: "KEYWORD_MARKEDBUILDING",
+        color: "secondary",
+        icon: "cube-outline"
+    },
+    {
+        text: "markierten Platz",
+        tag: "KEYWORD_MARKEDPLACE",
+        color: "secondary",
+        icon: "cube-outline"
+    },
+    {
+        text: "Straßenabschnitt",
+        tag: "KEYWORD_STREETSECTION",
+        color: "danger",
+        icon: "none"
+    },
+    {
+        text: "Straßenteil",
+        tag: "KEYWORD_STREETPART",
+        color: "danger",
+        icon: "none"
+    },
+    {
+        text: "Wegstück",
+        tag: "KEYWORD_ROUTESECTION",
+        color: "danger",
+        icon: "none"
+    },
+    {
+        text: "zurückgelegter Weg",
+        tag: "KEYWORD_WALKEDROUTE",
+        color: "danger",
+        icon: "none"
+    },
+    {
+        text: "Track",
+        tag: "KEYWORD_TRACK",
+        color: "danger",
+        icon: "none"
     }
 ]
