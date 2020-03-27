dc958dc4a912556e3541e16a1648d1bbe28e3cc5
felixerdy <felix.erdmann@uni-muenster.de>
2020-03-27 17:18:53 +0100

    fix

--- src/app/pipes/keywords.js
+++ src/app/pipes/keywords.js
@@ -2,85 +2,57 @@ export const mappings = [
     {
         text: "Fahne",
         tag: "KEYWORD_FLAG",
-        color: "primary",
-        icon: "navigate"
     },
     {
         text: "du bist hier",
         tag: "KEYWORD_HERE",
-        color: "danger",
-        icon: "radio-button-on"
     },
     {
         text: "marker",
         tag: "KEYWORD_MARKER",
-        color: "secondary",
-        icon: "location"
     },
     {
         text: "tippe auf die karte",
         tag: "KEYWORD_TAP_ON_MAP",
-        color: "secondary",
-        icon: "location"
     },
     {
         text: "tippe es an",
         tag: "KEYWORD_TAP_ON_IT",
-        color: "secondary",
-        icon: "location"
     },
     {
         text: "blickrichtung",
         tag: "KEYWORD_VIEW_DIRECTION",
-        color: "secondary",
-        icon: "caret-foreward"
     },
     {
         text: "markierte Haus",
         tag: "KEYWORD_MARKEDHOUSE",
-        color: "secondary",
-        icon: "cube-outline"
     },
     {
         text: "markierte Gebäude",
         tag: "KEYWORD_MARKEDBUILDING",
-        color: "secondary",
-        icon: "cube-outline"
     },
     {
         text: "markierten Platz",
         tag: "KEYWORD_MARKEDPLACE",
-        color: "secondary",
-        icon: "cube-outline"
     },
     {
         text: "Straßenabschnitt",
         tag: "KEYWORD_STREETSECTION",
-        color: "danger",
-        icon: "none"
     },
     {
         text: "Straßenteil",
         tag: "KEYWORD_STREETPART",
-        color: "danger",
-        icon: "none"
     },
     {
         text: "Wegstück",
         tag: "KEYWORD_ROUTESECTION",
-        color: "danger",
-        icon: "none"
     },
     {
         text: "zurückgelegter Weg",
         tag: "KEYWORD_WALKEDROUTE",
-        color: "danger",
-        icon: "none"
     },
     {
         text: "Track",
         tag: "KEYWORD_TRACK",
-        color: "danger",
-        icon: "none"
     }
 ]
