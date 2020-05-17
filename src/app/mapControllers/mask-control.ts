3ad8e4d62b46317562e8809a18fd77022111ba69
Yousef <yousef@gmx.de>
2020-05-17 16:39:19 +0200

    refactor #89

--- src/app/mapControllers/mask-control.ts
+++ src/app/mapControllers/mask-control.ts
@@ -31,46 +31,38 @@ export class MaskControl {
         });
         //this.map.setStyle('mapbox://styles/mapbox/streets-v7');
 
-        this.map.loadImage(
-            "/assets/icons/position.png",
-            (error, image) => {
-                if (error) throw error;
+        this.map.addSource("circle-mask", {
+            type: "geojson",
+            data: {
+                type: "Point",
+                coordinates: [
+                    7.626, 51.960
+                ]
+            }
+        });
 
-                this.map.addImage("geolocate", image);
+        this.map.addLayer({
+            id: "circle-mask",
+            source: "circle-mask",
+            type: "circle",
+            layout: {
+                'visibility': 'visible'
+            },
+            'paint': {
+                'circle-radius': 60/*  {
+                    stops: [[8, 10], [11, 6], [16, 40]]
+                } */,
+                //'circle-color': 'rgba(55,148,179,1)',
+                'circle-stroke-width': 800,
+                'circle-stroke-color': "#808080",
+                'circle-stroke-opacity': 0.5,
+                'circle-opacity': 0
+            }
+        });
 
-                this.map.addSource("circle-mask", {
-                    type: "geojson",
-                    data: {
-                        type: "Point",
-                        coordinates: [
-                            7.626, 51.960
-                        ]
-                    }
-                });
-
-                this.map.addLayer({
-                    id: "circle-mask",
-                    source: "circle-mask",
-                    type: "circle",
-                    layout: {
-                        'visibility': 'visible'
-                    },
-                    'paint': {
-                        'circle-radius': 60/*  {
-                            stops: [[8, 10], [11, 6], [16, 40]]
-                        } */,
-                        //'circle-color': 'rgba(55,148,179,1)',
-                        'circle-stroke-width': 800,
-                        'circle-stroke-color': "#808080",
-                        'circle-stroke-opacity': 0.5,
-                        'circle-opacity': 0
-                    }
-                });
-
-                this.map.setLayoutProperty('circle-mask', 'visibility', 'none');
-                this.isInitalized = true;
-                this.update()
-            });
+        this.map.setLayoutProperty('circle-mask', 'visibility', 'none');
+        this.isInitalized = true;
+        this.update()
     }
 
     public setType(type: MaskType): void {
