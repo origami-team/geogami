fa02bbd34331179cfb35f9fdfc86c7948e295010
YouQam <qamazyousef@gmail.com>
2020-10-08 13:08:02 +0200

    update show tasks' results view

--- src/app/pages/analyze-game/analyze-game-list/analyze-game-list.page.ts
+++ src/app/pages/analyze-game/analyze-game-list/analyze-game-list.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
-
 import { NavController } from "@ionic/angular";
-
 import { GamesService } from "../../../services/games.service";
 
 @Component({
@@ -11,6 +9,12 @@ import { GamesService } from "../../../services/games.service";
 })
 export class AnalyzeGameListPage implements OnInit {
   tracks: any[] = []
+  track: any[] = []
+  events: any[] = []
+  taskName: String = ""
+  game: any[] = []
+  AllGames: any[] = []
+
 
   constructor(
     public navCtrl: NavController,
@@ -20,7 +24,23 @@ export class AnalyzeGameListPage implements OnInit {
 
   ngOnInit() {
     this.gamesService.getTracks().then(tracks => {
-      this.tracks = tracks
+      if (tracks != null) {
+        tracks.forEach(element => {
+          this.taskName = element["name"]
+          this.track = element["events"]
+
+          this.track.forEach(event => {
+            if ((event["type"] == "ON_OK_CLICKED" && event["correct"] == true) || event["type"] == "WAYPOINT_REACHED") {
+              this.events.push(event)
+            }
+          });
+
+          this.tracks.push([this.taskName, this.events])
+          this.events = []
+        });
+
+        this.tracks  = JSON.parse(JSON.stringify(this.tracks))
+      }
     });
   }
 }
