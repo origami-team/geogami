4042825f9e8a162a843b25eee1b77fc534c9275d
felixerdy <felix.erdmann@uni-muenster.de>
2021-01-12 11:30:50 +0100

    show user information on profile page

--- src/app/pages/user/profile/profile.page.ts
+++ src/app/pages/user/profile/profile.page.ts
@@ -1,25 +1,29 @@
-import { Component, OnInit } from "@angular/core";
-import { AuthService } from "src/app/services/auth-service.service";
-import { Validators, FormBuilder } from "@angular/forms";
-import { NavController } from "@ionic/angular";
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { Validators, FormBuilder } from '@angular/forms';
+import { NavController } from '@ionic/angular';
 
 @Component({
-  selector: "app-profile",
-  templateUrl: "./profile.page.html",
-  styleUrls: ["./profile.page.scss"],
+  selector: 'app-profile',
+  templateUrl: './profile.page.html',
+  styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
   loginForm;
   error: string;
   register;
 
+  user;
+
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     public navCtrl: NavController
-  ) {}
+  ) { }
 
   ngOnInit(): void {
+    this.user = this.authService.getUserValue();
+
     this.loginForm = this.fb.group({
       username: [this.authService.getUserValue().username],
       email: [this.authService.getUserValue().email],
@@ -34,13 +38,13 @@ export class ProfilePage implements OnInit {
     });
   }
 
-  updateUser() {}
+  updateUser() { }
 
   logout() {
     this.authService.logout();
   }
 
   navigateRegister() {
-    this.navCtrl.navigateForward("user/register");
+    this.navCtrl.navigateForward('user/register');
   }
 }
