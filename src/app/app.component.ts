ad53fbe061b0e0454ddaee422716a600627d4dc9
felixerdy <felix.erdmann@uni-muenster.de>
2021-05-07 11:09:33 +0200

    implement apple AppTrackingTransparency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { Plugins, StatusBarStyle } from '@capacitor/core';
-import { AuthService } from './services/auth-service.service';
+import { Platform } from "@ionic/angular";
+import { Plugins, StatusBarStyle } from "@capacitor/core";
+import { AuthService } from "./services/auth-service.service";
+
+import { Response } from "capacitor-idfa-ios-app-tracking";
+const { IOSAppTracking } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
+  selector: "app-root",
+  templateUrl: "app.component.html",
 })
 export class AppComponent {
   constructor(private platform: Platform, private authService: AuthService) {
     this.initializeApp();
 
-    if (window.localStorage.getItem('bg_refreshtoken'))
+    if (window.localStorage.getItem("bg_refreshtoken"))
       this.authService.recoverSession(
-        window.localStorage.getItem('bg_refreshtoken')
+        window.localStorage.getItem("bg_refreshtoken")
       );
   }
 
@@ -23,9 +26,41 @@ export class AppComponent {
       Plugins.StatusBar.setStyle({ style: StatusBarStyle.Light }).catch((err) =>
         console.log(err)
       );
-      if (this.platform.is('android')) {
-        Plugins.StatusBar.setBackgroundColor({ color: 'white' });
+      if (this.platform.is("android")) {
+        Plugins.StatusBar.setBackgroundColor({ color: "white" });
       }
+
+      this.getTrackingPermission()
+        .then((res) => {
+          // User has allowed tracking.
+          // res.status is always "unrequested" here
+        })
+        .catch((err) => {
+          // User has denied tracking.
+        });
+    });
+  }
+
+  /**
+   * Requests tracking permission
+   */
+  getTrackingPermission(): Promise<Response> {
+    return new Promise((resolve, reject) => {
+      IOSAppTracking.getTrackingStatus().then((res: Response) => {
+        console.log("Tracking Status: " + res.status);
+        if (res.status === "unrequested") {
+          IOSAppTracking.requestPermission().then((response: Response) => {
+            console.log("Tracking Status: " + response.status);
+            resolve(response);
+          });
+        }
+        if (res.status === "authorized" || res.status === "restricted") {
+          resolve(res);
+        }
+        if (res.status === "denied") {
+          reject("Tracking permission denied");
+        }
+      });
     });
   }
 }
