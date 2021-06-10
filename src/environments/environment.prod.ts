78ecdcc62c85af1c996487de6272c3fbd76b3508
YouQam <qamazyousef@gmail.com>
2021-06-10 14:01:04 +0200

    update environment.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,10 @@ export const environment = {
   // apiURL: 'https://api.ori-gami.org',
   apiURL: 'https://api.origami.felixerdmann.com',
   mapboxAccessToken: 'pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA',
-  photoQuality: 10
+  photoQuality: 10,
+  // VR world
+  initialAvatarLoc: {
+    lng:224 / 111000,
+    lat:74 / 112000
+  }
 };
