ac28792adbb6c1c2346b3bdbee0a5dd8687a57e2
felixerdy <felix.erdmann@uni-muenster.de>
2021-10-28 13:13:01 +0200

    fix feedback

--- src/app/pages/create-game/form-elements/multiple-choice-text/multiple-choice-text.component.ts
+++ src/app/pages/create-game/form-elements/multiple-choice-text/multiple-choice-text.component.ts
@@ -1,25 +1,47 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-multiple-choice-text',
-  templateUrl: './multiple-choice-text.component.html',
-  styleUrls: ['./multiple-choice-text.component.scss'],
+  selector: "app-multiple-choice-text",
+  templateUrl: "./multiple-choice-text.component.html",
+  styleUrls: ["./multiple-choice-text.component.scss"],
 })
 export class MultipleChoiceTextComponent implements OnInit {
-
-  @Input() answers: String[] = ['', '', '', ''];
+  @Input() answers: String[] = ["", "", "", ""];
+  @Input() hints?: String[] = [
+    "Probiere es noch einmal.",
+    "Probiere es noch einmal.",
+    "Probiere es noch einmal.",
+    "Probiere es noch einmal.",
+  ];
 
   @Output() answersChange: EventEmitter<any> = new EventEmitter<any>();
+  @Output() hintsChange: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     if (this.answers == undefined) {
-      this.answers = ['', '', '', ''];
+      this.answers = ["", "", "", ""];
+    }
+    if (this.hints == undefined) {
+      this.hints = [
+        "Probiere es noch einmal.",
+        "Probiere es noch einmal.",
+        "Probiere es noch einmal.",
+        "Probiere es noch einmal.",
+      ];
     }
   }
 
   onChange() {
     this.answersChange.emit(this.answers);
+    this.hintsChange.emit(this.hints);
   }
 }
