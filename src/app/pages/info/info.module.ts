a4ca059fd04b1073f5abe68868f6ff18f4cdf16b
JaninaBistron <56539845+JaninaBistron@users.noreply.github.com>
2021-11-25 20:57:04 +0100

    Language Module
    First translations

--- src/app/pages/info/info.module.ts
+++ src/app/pages/info/info.module.ts
@@ -7,6 +7,9 @@ import { IonicModule } from '@ionic/angular';
 
 import { InfoPage } from './info.page';
 
+import { TranslateModule } from '@ngx-translate/core';
+
+
 const routes: Routes = [
   {
     path: '',
@@ -19,7 +22,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    TranslateModule
   ],
   declarations: [InfoPage]
 })
