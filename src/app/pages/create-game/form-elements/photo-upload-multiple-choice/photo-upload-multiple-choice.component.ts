3b8f4b59a78d588702900e1d220803a252b34574
Felix Erdmann <hello@felixerdmann.com>
2022-01-11 14:11:52 +0100

    fix multiple choice feedback

--- src/app/pages/create-game/form-elements/photo-upload-multiple-choice/photo-upload-multiple-choice.component.ts
+++ src/app/pages/create-game/form-elements/photo-upload-multiple-choice/photo-upload-multiple-choice.component.ts
@@ -28,6 +28,7 @@ export class PhotoUploadMultipleChoiceComponent implements OnInit {
     "Probiere es noch einmal.",
     "Probiere es noch einmal.",
   ];
+  @Input() feedback: boolean;
 
   @Output() photosChange: EventEmitter<any> = new EventEmitter<any>();
   @Output() hintsChange: EventEmitter<any> = new EventEmitter<any>();
