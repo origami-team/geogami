79df8391375b7dd4bd5d4043ae4a775c81298d04
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2022-11-17 10:27:18 +0100

    update android version

--- android/app/src/main/java/com/ifgi/geogami/MainActivity.java
+++ android/app/src/main/java/com/ifgi/geogami/MainActivity.java
@@ -7,11 +7,6 @@ import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
 
-import com.go.capacitor.keepscreenon.CapacitorKeepScreenOn;
-import com.tchvu3.capvoicerecorder.VoiceRecorder;
-
-
-
 public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -21,8 +16,6 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(CapacitorKeepScreenOn.class);
-      add(VoiceRecorder.class); // Add this line
     }});
   }
 }
