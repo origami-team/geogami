2e6b301c4999af7f37ce346e113da1b08f5bd0e2
Yousef Qamaz <40634685+YouQam@users.noreply.github.com>
2023-01-18 09:55:20 +0100

    add barcode scanner for android

--- android/app/src/main/java/com/ifgi/geogami/MainActivity.java
+++ android/app/src/main/java/com/ifgi/geogami/MainActivity.java
@@ -6,11 +6,7 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-
-import com.go.capacitor.keepscreenon.CapacitorKeepScreenOn;
-import com.tchvu3.capvoicerecorder.VoiceRecorder;
-
-
+import com.dutchconcepts.capacitor.barcodescanner.BarcodeScanner;
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -21,8 +17,7 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(CapacitorKeepScreenOn.class);
-      add(VoiceRecorder.class); // Add this line
+      add(BarcodeScanner.class);
     }});
   }
 }
