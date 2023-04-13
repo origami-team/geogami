f1411569fb99b0bf12f57bd71184c6fb2d8bfdb2
YouQam <qamazyousef@gmail.com>
2023-04-13 17:24:27 +0200

    some updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiURL: "https://api.origami.felixerdmann.com",
-  apiURL: "http://localhost:3001",
+  apiURL: "https://api.origami.felixerdmann.com",
+  // apiURL: "http://localhost:3001",
   mapboxAccessToken:
     "pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA",
   photoQuality: 10,
