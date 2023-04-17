efaab94aa463b0f4b02ce7b340ea39400d581579
YouQam <qamazyousef@gmail.com>
2023-04-17 13:19:48 +0200

    update zoom level of zoom tasks

--- src/app/pages/play-game/playing-game/playing-game.page.ts
+++ src/app/pages/play-game/playing-game/playing-game.page.ts
@@ -894,9 +894,9 @@ export class PlayingGamePage implements OnInit, OnDestroy {
       // console.log("🚀 ~ PlayingGamePage111 ~ this.map.on ~ currentZoom:", currentZoom)
       /* (V.E.): each vir env. has a zoom 0 layer, this is for those which has another layer that is visible to show more details */
       if (environment.virEnvProperties[this.virEnvType].zoomInLayer1) {
-        if (currentZoom <= 18.5 && this.map.getStyle().sources.overlay.url != "assets/vir_envs_layers/" + this.virEnvType + ".png") {
+        if (currentZoom <= 19.2 && this.map.getStyle().sources.overlay.url != "assets/vir_envs_layers/" + this.virEnvType + ".png") {
           this.updateMapStyleOverlayLayer("assets/vir_envs_layers/" + this.virEnvType + ".png", false);
-        } else if (currentZoom > 18.5 && this.map.getStyle().sources.overlay.url != "assets/vir_envs_layers/" + this.virEnvType + "b.png") {
+        } else if (currentZoom > 19.2 && this.map.getStyle().sources.overlay.url != "assets/vir_envs_layers/" + this.virEnvType + "b.png") {
           this.updateMapStyleOverlayLayer("assets/vir_envs_layers/" + this.virEnvType + "_zoom1.png", false);
         }
       }
