dfe322423605fa81a1d4848dd58a502d1ab339dd
YouQam <qamazyousef@gmail.com>
2023-10-09 15:06:32 +0200

    update authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,207 +1,236 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './services/auth-gard.service';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./services/auth-gard.service";
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     loadChildren: () =>
-      import('./start/start.module').then((m) => m.StartPageModule),
+      import("./start/start.module").then((m) => m.StartPageModule),
   },
   {
-    path: 'play-game/games-overview',
+    path: "play-game/games-overview",
     loadChildren: () =>
-      import('./pages/play-game/games-overview/games-overview.module').then(
+      import("./pages/play-game/games-overview/games-overview.module").then(
         (m) => m.GamesOverviewPageModule
       ),
   },
   {
-    path: 'play-game/play-game-list',
+    path: "play-game/play-game-list",
     loadChildren: () =>
-      import('./pages/play-game/play-game-list/play-game-list.module').then(
+      import("./pages/play-game/play-game-list/play-game-list.module").then(
         (m) => m.PlayGameListPageModule
       ),
   },
   {
-    path: 'play-game/game-detail/:id',
+    path: "play-game/game-detail/:id",
     loadChildren: () =>
-      import('./pages/play-game/game-detail/game-detail.module').then(
+      import("./pages/play-game/game-detail/game-detail.module").then(
         (m) => m.GameDetailPageModule
       ),
   },
   {
-    path: 'play-game/playing-game/:bundle',
+    path: "play-game/playing-game/:bundle",
     loadChildren: () =>
-      import('./pages/play-game/playing-game/playing-game.module').then(
+      import("./pages/play-game/playing-game/playing-game.module").then(
         (m) => m.PlayingGamePageModule
       ),
   },
   {
-    path: 'create-game/:bundle',
+    path: "create-game/:bundle",
     loadChildren: () =>
-      import('./pages/create-game/create-game/create-game.module').then(
+      import("./pages/create-game/create-game/create-game.module").then(
         (m) => m.CreateGamePageModule
       ),
     canActivate: [AuthGuard],
   },
   {
-    path: 'create-game/create-game-overview/:bundle',
+    path: "create-game/create-game-overview/:bundle",
     loadChildren: () =>
       import(
-        './pages/create-game/create-game-overview/create-game-overview.module'
+        "./pages/create-game/create-game-overview/create-game-overview.module"
       ).then((m) => m.CreateGameOverviewPageModule),
     canActivate: [AuthGuard],
   },
   {
-    path: 'create-game-list/:bundle',
+    path: "create-game-list/:bundle",
     loadChildren: () =>
       import(
-        './pages/create-game/create-game-list/create-game-list.module'
+        "./pages/create-game/create-game-list/create-game-list.module"
       ).then((m) => m.CreateGameListPageModule),
     canActivate: [AuthGuard],
   },
   {
-    path: 'create-game/create-game-map',
+    path: "create-game/create-game-map",
     loadChildren: () =>
-      import('./pages/create-game/create-game-map/create-game-map.module').then(
+      import("./pages/create-game/create-game-map/create-game-map.module").then(
         (m) => m.CreateGameMapPageModule
       ),
     canActivate: [AuthGuard],
   },
   {
-    path: 'edit-game-list',
+    path: "edit-game-list",
     loadChildren: () =>
-      import('./pages/edit-game/edit-game-list/edit-game-list.module').then(
+      import("./pages/edit-game/edit-game-list/edit-game-list.module").then(
         (m) => m.EditGameListPageModule
       ),
     canActivate: [AuthGuard],
   },
   {
-    path: 'edit-game-tasks/:bundle',
+    path: "edit-game-tasks/:bundle",
     loadChildren: () =>
-      import('./pages/edit-game/edit-game-tasks/edit-game-tasks.module').then(
+      import("./pages/edit-game/edit-game-tasks/edit-game-tasks.module").then(
         (m) => m.EditGameTasksPageModule
       ),
     canActivate: [AuthGuard],
   },
   {
-    path: 'edit-game/edit-game-overview/:bundle',
+    path: "edit-game/edit-game-overview/:bundle",
     loadChildren: () =>
       import(
-        './pages/edit-game/edit-game-overview/edit-game-overview.module'
+        "./pages/edit-game/edit-game-overview/edit-game-overview.module"
       ).then((m) => m.EditGameOverviewPageModule),
     canActivate: [AuthGuard],
   },
   {
-    path: 'analyze',
+    path: "analyze",
     loadChildren: () =>
       import(
-        './pages/analyze-game/analyze-game-list/analyze-game-list.module'
+        "./pages/analyze-game/analyze-game-list/analyze-game-list.module"
       ).then((m) => m.AnalyzeGameListPageModule),
+    canActivate: [AuthGuard],
   },
   {
-    path: 'user/login',
+    path: "user/login",
     loadChildren: () =>
-      import('./pages/user/login/login.module').then((m) => m.LoginPageModule),
+      import("./pages/user/login/login.module").then((m) => m.LoginPageModule),
   },
   {
-    path: 'user/register',
+    path: "user/register",
     loadChildren: () =>
-      import('./pages/user/register/register.module').then(
+      import("./pages/user/register/register.module").then(
         (m) => m.RegisterPageModule
       ),
   },
   {
-    path: 'user/profile',
+    path: "user/profile",
     loadChildren: () =>
-      import('./pages/user/profile/profile.module').then(
+      import("./pages/user/profile/profile.module").then(
         (m) => m.ProfilePageModule
       ),
     canActivate: [AuthGuard],
   },
   {
-    path: 'user/forgot-password',
+    path: "user/forgot-password",
     loadChildren: () =>
-      import('./pages/user/forgot-password/forgot-password.module').then(
+      import("./pages/user/forgot-password/forgot-password.module").then(
         (m) => m.ForgotPasswordPageModule
       ),
   },
   {
-    path: 'user/reset-password',
+    path: "user/reset-password",
     loadChildren: () =>
-      import('./pages/user/reset-password/reset-password.module').then(
+      import("./pages/user/reset-password/reset-password.module").then(
         (m) => m.ResetPasswordPageModule
       ),
+    canActivate: [AuthGuard],
   },
   // older stuff
   // { path: 'map-swipe', loadChildren: './map-swipe/map-swipe.module#MapSwipePageModule' },
   {
-    path: 'start',
+    path: "start",
     loadChildren: () =>
-      import('./start/start.module').then((m) => m.StartPageModule),
+      import("./start/start.module").then((m) => m.StartPageModule),
   },
   {
-    path: 'map-showroom',
+    path: "map-showroom",
     loadChildren: () =>
-      import('./pages/showroom/map-showroom/map-showroom.module').then(
+      import("./pages/showroom/map-showroom/map-showroom.module").then(
         (m) => m.MapShowroomPageModule
       ),
   },
   {
-    path: 'info',
+    path: "info",
     loadChildren: () =>
-      import('./pages/info/info.module').then((m) => m.InfoPageModule),
+      import("./pages/info/info.module").then((m) => m.InfoPageModule),
   },
   {
-    path: 'showroom',
+    path: "showroom",
     loadChildren: () =>
-      import('./pages/showroom/showroom/showroom.module').then(
+      import("./pages/showroom/showroom/showroom.module").then(
         (m) => m.ShowroomPageModule
       ),
   },
   {
-    path: 'task-showroom',
+    path: "task-showroom",
     loadChildren: () =>
-      import('./pages/showroom/task-showroom/task-showroom.module').then(
+      import("./pages/showroom/task-showroom/task-showroom.module").then(
         (m) => m.TaskShowroomPageModule
       ),
   },
   {
-    path: 'create-game-menu',
-    loadChildren: () => import('./pages/create-game/create-game-menu/create-game-menu.module').then( m => m.CreateGameMenuPageModule)
+    path: "create-game-menu",
+    loadChildren: () =>
+      import(
+        "./pages/create-game/create-game-menu/create-game-menu.module"
+      ).then((m) => m.CreateGameMenuPageModule),
   },
   {
-    path: 'create-game-virtual',
-    loadChildren: () => import('./pages/create-game/create-game-virtual/create-game-virtual.module').then( m => m.CreateGameVirtualPageModule)
+    path: "create-game-virtual",
+    loadChildren: () =>
+      import(
+        "./pages/create-game/create-game-virtual/create-game-virtual.module"
+      ).then((m) => m.CreateGameVirtualPageModule),
   },
   {
-    path: 'create-game-virtual-list/:VR_version',
-    loadChildren: () => import('./pages/create-game/create-game-virtual-list/create-game-virtual-list.module').then( m => m.CreateGameVirtualListPageModule)
+    path: "create-game-virtual-list/:VR_version",
+    loadChildren: () =>
+      import(
+        "./pages/create-game/create-game-virtual-list/create-game-virtual-list.module"
+      ).then((m) => m.CreateGameVirtualListPageModule),
   },
   {
-    path: 'play-game/play-game-menu',
-    loadChildren: () => import('./pages/play-game/play-game-menu/play-game-menu.module').then( 
-      m => m.PlayGameMenuPageModule)
+    path: "play-game/play-game-menu",
+    loadChildren: () =>
+      import("./pages/play-game/play-game-menu/play-game-menu.module").then(
+        (m) => m.PlayGameMenuPageModule
+      ),
   },
   {
-    path: 'create-game-virtual-menu/:bundle',
-    loadChildren: () => import('./pages/create-game/create-game-virtual-menu/create-game-virtual-menu.module').then( m => m.CreateGameVirtualMenuPageModule)
+    path: "create-game-virtual-menu/:bundle",
+    loadChildren: () =>
+      import(
+        "./pages/create-game/create-game-virtual-menu/create-game-virtual-menu.module"
+      ).then((m) => m.CreateGameVirtualMenuPageModule),
   },
   {
-    path: 'user/user-management',
-    loadChildren: () => import('./pages/user/user-management/user-management.module').then( m => m.UserManagementPageModule)
+    path: "user/user-management",
+    loadChildren: () =>
+      import("./pages/user/user-management/user-management.module").then(
+        (m) => m.UserManagementPageModule
+      ),
+    canActivate: [AuthGuard],
   },
   {
-    path: 'game-type-menu/:worldType',
-    loadChildren: () => import('./pages/multiplayer/game-type-menu/game-type-menu.module').then( m => m.GameTypeMenuPageModule)
+    path: "game-type-menu/:worldType",
+    loadChildren: () =>
+      import("./pages/multiplayer/game-type-menu/game-type-menu.module").then(
+        (m) => m.GameTypeMenuPageModule
+      ),
   },
   {
-    path: 'barcode-scanner',
-    loadChildren: () => import('./pages/barcode-scanner/barcode-scanner.module').then( m => m.BarcodeScannerPageModule)
+    path: "barcode-scanner",
+    loadChildren: () =>
+      import("./pages/barcode-scanner/barcode-scanner.module").then(
+        (m) => m.BarcodeScannerPageModule
+      ),
   },
   {
-    path: 'analyze/game-tracks/:bundle',
-    loadChildren: () => import('./pages/analyze-game/game-tracks/game-tracks.module').then( m => m.GameTracksPageModule)
+    path: "analyze/game-tracks/:bundle",
+    loadChildren: () =>
+      import("./pages/analyze-game/game-tracks/game-tracks.module").then(
+        (m) => m.GameTracksPageModule
+      ),
   },
 ];
 @NgModule({
