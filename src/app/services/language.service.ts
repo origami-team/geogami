bd87f5cb13e430c56e4934428676e24e0114e0f8
YouQam <qamazyousef@gmail.com>
2023-10-12 13:45:05 +0200

    use language keywords instead of flags

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -36,9 +36,9 @@ export class LanguageService {
 
   getLangauges() {
     return [
-      { value: 'de', img: '🇩🇪', text: 'Deutsch' },
-      { value: 'en', img: '🇺🇸', text: 'English' },
-      { value: 'ar', img: '🇪🇬', text: 'Arabic' },
+      { value: 'de', img: 'DE', text: 'Deutsch' },
+      { value: 'en', img: 'EN', text: 'English' },
+      { value: 'ar', img: 'AR', text: 'Arabic' },
     ];
   }
 
