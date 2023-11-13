302af1bbb8734ba579d45359df4362746e354259
YouQam <qamazyousef@gmail.com>
2023-11-13 19:18:27 +0100

    code refactoring

--- src/app/pages/analyze-game/game-tracks-visualization/game-tracks-visualization.module.ts
+++ src/app/pages/analyze-game/game-tracks-visualization/game-tracks-visualization.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
-import { GameTracksVisualizationPageRoutingModule } from './game-tracks-visualization-routing.module';
-
 import { GameTracksVisualizationPage } from './game-tracks-visualization.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: GameTracksVisualizationPage
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    GameTracksVisualizationPageRoutingModule
-  ],
+    RouterModule.forChild(routes)
+    ],
   declarations: [GameTracksVisualizationPage]
 })
 export class GameTracksVisualizationPageModule {}
--- src/app/pages/analyze-game/game-tracks/game-tracks.module.ts
+++ src/app/pages/analyze-game/game-tracks/game-tracks.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { Routes, RouterModule } from '@angular/router';
+
 
 import { IonicModule } from "@ionic/angular";
-
-import { GameTracksPageRoutingModule } from "./game-tracks-routing.module";
 
 import { GameTracksPage } from "./game-tracks.page";
 import { TranslateModule } from "@ngx-translate/core";
 import { MaterialModule } from "src/app/material.module";
 import { downloadTrackDialog } from "../../../components/download-track-dialog/download-track-dialog";
 
+const routes: Routes = [
+  {
+    path: '',
+    component: GameTracksPage
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    GameTracksPageRoutingModule,
+    RouterModule.forChild(routes),
     TranslateModule,
     MaterialModule,
   ],
