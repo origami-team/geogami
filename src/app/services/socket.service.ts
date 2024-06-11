85a58292fe4f9737d35c850b86ecaa76ecc6af1d
YouQam <qamazyousef@gmail.com>
2024-06-11 12:35:00 +0200

    add socket functions for create and join rooms -single player

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -10,4 +10,18 @@ export class SocketService {
   constructor(socket: Socket) { 
     this.socket = socket;
   }
+
+  creatAndJoinNewRoom(gameCode:string, virEnvType:string, isSingleMode:boolean){
+    this.socket.emit("newGame", {
+      gameCode: gameCode,
+      virEnvType: virEnvType,
+      isSingleMode: isSingleMode,
+    });
+  }
+
+  joinVERoom(gameCode:string){
+    this.socket.emit("joinVEGame", {
+      gameCode: gameCode,
+    });
+  }
 }
