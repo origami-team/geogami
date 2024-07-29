9befbf03083474566a474aa4b14c629630008bc5
YouQam <qamazyousef@gmail.com>
2024-07-29 16:31:14 +0200

    feat: add translation

--- src/app/components/env-type-question-text/env-type-question-text.module.ts
+++ src/app/components/env-type-question-text/env-type-question-text.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { EnvTypeQuestionTextComponent } from './env-type-question-text.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 
 @NgModule({
     imports: [
         CommonModule,
-        IonicModule
+        IonicModule,
+        TranslateModule
     ],
     declarations: [EnvTypeQuestionTextComponent],
     providers: [],
