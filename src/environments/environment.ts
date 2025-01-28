daddf074d31534bf4952ed76e10b36596b53a394
YouQam <qamazyousef@gmail.com>
2025-01-28 14:38:23 +0100

    feat: create duplicate flag only with swipe map-feature

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 // const localhost="localhost"
-const localhostIP="00.00.00.000";
-const localWebGLPort="8080";
+const localhostIP="192.168.178.23";
+const localWebGLPort="57275";
 
 export const environment = {
   production: false,
   apiURL: `http://${localhostIP}:3000`,
   uiURL:`http://${localhostIP}:8100`,
-  webglURL: `http://${localhostIP}:${localWebGLPort}`,
+  webglURL: `http://localhost:${localWebGLPort}`,
   mapboxAccessToken:
     "pk.eyJ1IjoiZmVsaXhhZXRlbSIsImEiOiI2MmE4YmQ4YjIzOTI2YjY3ZWFmNzUwOTU5NzliOTAxOCJ9.nshlehFGmK_6YmZarM2SHA",
   photoQuality: 10,
